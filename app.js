523009fdc479a49bf173598a1188cf606b9d093a
kovalyovdsMac <kovalyovds@gmail.com>
2022-06-28 13:49:29 +0300

    lesson_01_additional

--- app.js
+++ app.js
@@ -1,53 +1,26 @@
-// const fs = require('fs');
-// const path = require('path');
-//
-// const sortFolder = (read, gender, write) => {
-//     fs.readdir(path.join(__dirname, read), (err, files) => {
-//         if (err) return console.log(err);
-//
-//         files.forEach((file) => {
-//             const redFolderPath = path.join(__dirname, read, file)
-//             fs.readFile(redFolderPath, (err, data) => {
-//                 if (err) return console.log(err);
-//
-//                 const user = JSON.parse(data.toString());
-//
-//                 if (user.gender === gender) {
-//                     fs.rename(redFolderPath, path.join(__dirname, write, file), (err) => {
-//                         if (err) return console.log(err);
-//                     });
-//                 }
-//
-//             });
-//         });
-//     });
-// };
-//
-// sortFolder('girls', 'male', 'boys');
-// sortFolder('boys', 'female', 'girls');
-
-//Promises
-
 const fs = require('fs/promises');
 const path = require('path');
 
-const sortFolder = async (read, gender, write) => {
+const sortFolder = async (read) => {
     try {
-        const files = await fs.readdir(path.join(__dirname, read))
+        const readPathFolder = path.join(__dirname, read);
+        const files = await fs.readdir(readPathFolder);
 
         for (const file of files) {
-            const redFolderPath = path.join(__dirname, read, file)
-            const data = await fs.readFile(redFolderPath)
-            const user = JSON.parse(data.toString());
+            const stat = await fs.stat(path.join(readPathFolder, file))
 
-            if (user.gender === gender) {
-                await fs.rename(redFolderPath, path.join(__dirname, write, file));
+            if (stat.isFile()) {
+                await fs.rename(path.join(read, file), path.join(__dirname, 'exitFolder', file));
             }
+
+            if (stat.isDirectory()) {
+                await sortFolder(path.join(read, file))
+            }
+
         }
     } catch (e) {
         console.error(e);
     }
 };
 
-sortFolder('girls', 'male', 'boys');
-sortFolder('boys', 'female', 'girls');
+sortFolder('allFiles');
